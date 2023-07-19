bb272dd61e3e22a69868ccac29112b000ae24e5a
Musharraf188 <mr.musharraf188@gmail.com>
2023-07-19 17:13:55 +0530

    InvestForm/Created

--- src/components/InvestForm.js
+++ src/components/InvestForm.js
@@ -1,27 +1,39 @@
-import React from "react";
-
+import React, { useState } from "react";
+const intialState={
+  'current-savings':10000,
+  'yearly-contribution':1200,
+  'expected-return': 7,
+  'duration':10
+}
+ 
 const InvestForm =()=>{
+  const [userInput, setUserInput] = useState(intialState);
     const submitHandler=(event)=>{
         event.preventDefault();
         //.. 
-        console.log('SUBMIT')
+        console.log('SUBMIT');
     }
-    const clickHandler =()=>{
-        //..
+    const resetHandler =()=>{
+      setUserInput(intialState);
         console.log('RESET')
     }
     const inputHandler=(index, value)=>{
-   console.log(index, value)
+  setUserInput((prevValue)=>{
+    return {
+      ...prevValue,
+      [index]:value
+    }
+  });
     }
     return  <form onSubmit={submitHandler} className="form">
     <div className="input-group">
       <p>
         <label htmlFor="current-savings">Current Savings ($)</label>
-        <input onChange={(event)=>{inputHandler('current-savings', event.target.value)}} type="number" id="current-savings" />
+        <input value={userInput['current-savings']} onChange={(event)=>{inputHandler('current-savings', event.target.value)}} type="number" id="current-savings" />
       </p>
       <p>
         <label htmlFor="yearly-contribution">Yearly Savings ($)</label>
-        <input  onChange={(event)=>{inputHandler('yearly-contribution', event.target.value)}} type="number" id="yearly-contribution" />
+        <input  value={userInput['yearly-contribution']} onChange={(event)=>{inputHandler('yearly-contribution', event.target.value)}} type="number" id="yearly-contribution" />
       </p>
     </div>
     <div className="input-group">
@@ -29,15 +41,15 @@ const InvestForm =()=>{
         <label htmlFor="expected-return">
           Expected Interest (%, per year)
         </label>
-        <input  onChange={(event)=>{inputHandler('expected-return', event.target.value)}} type="number" id="expected-return" />
+        <input value={userInput['expected-return']} onChange={(event)=>{inputHandler('expected-return', event.target.value)}} type="number" id="expected-return" />
       </p>
       <p>
         <label htmlFor="duration">Investment Duration (years)</label>
-        <input  onChange={(event)=>{inputHandler('duration', event.target.value)}} type="number" id="duration" />
+        <input value={userInput['duration']}  onChange={(event)=>{inputHandler('duration', event.target.value)}} type="number" id="duration" />
       </p>
     </div>
     <p className="actions">
-      <button onClick={clickHandler} type="reset" className="buttonAlt">
+      <button onClick={resetHandler} type="reset" className="buttonAlt">
         Reset
       </button>
       <button type="submit" className="button">
