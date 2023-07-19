5b9d066a1b583438b05e34c2c1ba33ce6c554bd0
Musharraf188 <mr.musharraf188@gmail.com>
2023-07-19 12:08:32 +0530

    split

--- src/components/InvestForm.js
+++ src/components/InvestForm.js
@@ -10,15 +10,18 @@ const InvestForm =()=>{
         //..
         console.log('RESET')
     }
+    const inputHandler=(index, value)=>{
+   console.log(index, value)
+    }
     return  <form onSubmit={submitHandler} className="form">
     <div className="input-group">
       <p>
         <label htmlFor="current-savings">Current Savings ($)</label>
-        <input type="number" id="current-savings" />
+        <input onChange={(event)=>{inputHandler('current-savings', event.target.value)}} type="number" id="current-savings" />
       </p>
       <p>
         <label htmlFor="yearly-contribution">Yearly Savings ($)</label>
-        <input type="number" id="yearly-contribution" />
+        <input  onChange={(event)=>{inputHandler('yearly-contribution', event.target.value)}} type="number" id="yearly-contribution" />
       </p>
     </div>
     <div className="input-group">
@@ -26,11 +29,11 @@ const InvestForm =()=>{
         <label htmlFor="expected-return">
           Expected Interest (%, per year)
         </label>
-        <input type="number" id="expected-return" />
+        <input  onChange={(event)=>{inputHandler('expected-return', event.target.value)}} type="number" id="expected-return" />
       </p>
       <p>
         <label htmlFor="duration">Investment Duration (years)</label>
-        <input type="number" id="duration" />
+        <input  onChange={(event)=>{inputHandler('duration', event.target.value)}} type="number" id="duration" />
       </p>
     </div>
     <p className="actions">
