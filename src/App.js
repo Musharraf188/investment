980080aa52c99971bfe1172e1857a3a6af25d6a5
Musharraf188 <mr.musharraf188@gmail.com>
2023-08-01 13:47:17 +0530

    doo

--- src/App.js
+++ src/App.js
@@ -1,20 +1,25 @@
+import { useState } from 'react';
 import logo from './assets/investment-calculator-logo.png';
 import Header from './components/Header';
 import ResultTable from './components/ResultTable';
 import UserForm from './components/UserForm';
 
 function App() {
+  const [userInput, setInput] = useState(null);
   const calculateHandler = (userInput) => {
     // Should be triggered when form is submitted
     // You might not directly want to bind it to the submit event on the form though...
+setInput(userInput);
 
-    const yearlyData = []; // per-year results
-
+    // do something with yearlyData ...
+  };
+  const yearlyData = []; // per-year results
+  if(userInput){
     let currentSavings = +userInput['current-savings']; // feel free to change the shape of this input object!
     const yearlyContribution = +userInput['yearly-contribution']; // as mentioned: feel free to change the shape...
     const expectedReturn = +userInput['expected-return'] / 100;
     const duration = +userInput['duration'];
-
+  
     // The below code calculates yearly results (total savings, interest etc)
     for (let i = 0; i < duration; i++) {
       const yearlyInterest = currentSavings * expectedReturn;
@@ -26,18 +31,19 @@ function App() {
         savingsEndOfYear: currentSavings,
         yearlyContribution: yearlyContribution,
       });
-    }
+  
+  }
+ 
 
-    // do something with yearlyData ...
-  };
+   }
 
   return (
     <div>
      <Header />
-      <UserForm />
+      <UserForm onCalculate={calculateHandler}/>
       {/* Todo: Show below table conditionally (only once result data is available) */}
       {/* Show fallback text if no data is available */}
-<ResultTable />
+<ResultTable data={yearlyData}/>
        </div>
   );
 }
